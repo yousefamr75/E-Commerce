69722c707219eaa8b3aec888a3633fd8979f159b
yousefamr75 <yousefamr200444@gmail.com>
2024-10-17 15:45:33 +0300

    Add files via upload

---
+++ wwwroot/js/products.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function ()){
+    loadDataTable();
+}
+function loadDataTable{
+    datatable = $('#tblData').DataTable({
+        "ajax": { URL: '/Admin/product/getall' },
+   
+        " columns": [
+        { data: 'name' , "with" : "15%"},
+            { data: 'position', "with": "15%" },
+            { data: 'salary', "with": "15%" },
+            { data: 'office', "with": "15%" }
+
+
+
+    ]);
+}
+
+}
+
